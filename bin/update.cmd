06f72a7fb075023686de4c9022fa94178deeaf27
mike <>
2013-10-04 19:18:59 +0000

    Updating works on windows

--- bin/update.cmd
+++ bin/update.cmd
@@ -1,6 +1,9 @@
 %~d0
 cd %~dp0
 set BUILD_VERSION=0.4.17-dev1
+mkdir ..\download
 wget -O ../download/spiralcraft-sandbox-%BUILD_VERSION%.zip http://publish.spiralcraft.com/snapshot/spiralcraft/distrib-sandbox/%BUILD_VERSION%/spiralcraft-sandbox-%BUILD_VERSION%.zip 
-unzip -Xo ../download/spiralcraft-sandbox-%BUILD_VERSION%.zip -d ..
+copy unzip.exe unzip.tmp.exe
+unzip.tmp -Xo ../download/spiralcraft-sandbox-%BUILD_VERSION%.zip -d ..
+del unzip.tmp.exe
 pause
