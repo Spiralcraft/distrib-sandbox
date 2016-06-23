c14bde7f913222dcc4b044f98fa163353c3b56d1
mike <mike.git@spiralcraft.com>
2016-06-23 12:35:08 -0700

    Update internal version

--- bin/update.cmd
+++ bin/update.cmd
@@ -1,6 +1,6 @@
 %~d0
 cd %~dp0
-set BUILD_VERSION=0.4.17-dev1
+set BUILD_VERSION=0.4.17-dev4
 mkdir ..\download
 wget -O ../download/spiralcraft-sandbox-%BUILD_VERSION%.zip http://publish.spiralcraft.com/snapshot/spiralcraft/distrib-sandbox/%BUILD_VERSION%/spiralcraft-sandbox-%BUILD_VERSION%.zip 
 copy unzip.exe unzip.tmp.exe
